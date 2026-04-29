b3a807f603b55aeda97de15c93198af641326999
Edwin659 <gwennaelle.airo-farulla@isen.yncrea.fr>
2026-04-30 00:35:16 +0900

    Player's Health & Camera fixes
    Player's Health & Camera fixes

--- Assets/Scripts/Combat_System/PlayerHealth.cs
+++ Assets/Scripts/Combat_System/PlayerHealth.cs
@@ -1,22 +1,67 @@
 using UnityEngine;
+using UnityEngine.InputSystem.Processors;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 100;
-    public int health;
+    public int maxLife = 3;
+    public int currentLife;
+    public int currentHealth;
+    private Animator playerAnim;
+    public Slider healthSlider;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        health = maxHealth;
+        currentHealth = maxHealth;
+        currentLife = maxLife;
+        playerAnim = GetComponent<Animator>();
+        healthSlider.value = currentHealth;
     }
 
     public void TakeDamage(int damage)
     {
-        health -= damage;
-        if (health <= 0)
+        currentHealth -= damage;
+        healthSlider.value = currentHealth;
+        if (currentHealth <= 0)
         {
-            Destroy(gameObject);
+            currentHealth = 0;
+            Die();
+            //Bonus if time (health = checkpoints) (life= level)
+            //currentLife--;
+            //if (currentLife <= 0){
+                //NoLife();
+            //}
+            //else
+            //{
+                //UI at the begining 
+                //UnityEngine.SceneManagement.SceneManager.LoadScene("");
+            //}
         }
+    }
+    public void Heal(int healing)
+    {
+        currentHealth += healing;
+        if (currentHealth >= maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        healthSlider.value = currentHealth;
+    }
+    public void Die() {
+        Debug.Log("Player is dead");
+        playerAnim.SetTrigger("IsDead");
+        GetComponent<PlayerMovement>().enabled = false; // stop movement
+    }
+    public void DieEnd()
+    {
+        // Game Over Scene
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("GameOverScene");
+    }
+    public void NoLife() //bonus
+    {
+        // Game Over Scene
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("GameOverScene");
     }
 }
