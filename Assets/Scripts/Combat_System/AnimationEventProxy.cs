ccd648f7aa1e65603c5cf7c75c462f78ec21de4f
Edwin659 <gwennaelle.airo-farulla@isen.yncrea.fr>
2026-06-09 22:39:56 +0900

    commantary

--- Assets/Scripts/Combat_System/AnimationEventProxy.cs
+++ Assets/Scripts/Combat_System/AnimationEventProxy.cs
@@ -4,7 +4,6 @@ public class AnimationEventProxy : MonoBehaviour
 {
     public void DealDamage()
     {
-        // Cherche le script PlayerDamage sur le parent
         PlayerDamage playerDamage = GetComponentInParent<PlayerDamage>();
         if (playerDamage != null)
         {
